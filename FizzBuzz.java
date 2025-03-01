e911cf6dff1053c7f831d56cc803e5d6f2aed451
prakul19 <prakulagarwal5@gmail.com>
2025-03-01 13:11:42 +0530

    [prakul] stats: program for printing fizz and buzz for multiples of 3 and 5

---
+++ FizzBuzz.java
@@ -0,0 +1,29 @@
+class FizzBuzz {
+    public static void main(String[] args) {
+        java.util.Scanner input = new java.util.Scanner(System.in);
+
+        // Take user input for the number
+        System.out.print("Enter a positive integer: ");
+        int number = input.nextInt();
+
+        // Check if the number is positive
+        if (number > 0) {
+            // Iterate from 1 to the number
+            for (int i = 1; i <= number; i++) {
+                // Check for FizzBuzz, Fizz, or Buzz
+                if (i % 3 == 0 && i % 5 == 0) {
+                    System.out.println("FizzBuzz");
+                } else if (i % 3 == 0) {
+                    System.out.println("Fizz");
+                } else if (i % 5 == 0) {
+                    System.out.println("Buzz");
+                } else {
+                    System.out.println(i);
+                }
+            }
+        } else {
+            System.out.println("The number is not a positive integer");
+        }
+    }
+}
+
